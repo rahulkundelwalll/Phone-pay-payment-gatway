6681aa64513bfa953fb62afc7c8aa59a65bf1ed8
shivani <2020ume0227@iitjammu.ac.in>
2024-04-23 03:02:24 +0530

    api-integration

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import 'dotenv/config'
+import 'dotenv/config';
+import axios from "axios";
 
 const app = express();
 
@@ -8,6 +9,35 @@ const app = express();
 app.get("/",(req,res)=>{
     res.send("hello world");
 })
+
+app.get("/pay",(req,res)=>{
+    const payEnd = "/pg/v1/pay";
+
+
+    
+
+const options = {
+  method: 'post',
+  url: `${process.env.PHONE_PE_HOST_URL}${payEnd}`,
+  headers: {
+        accept: 'text/plain',
+        'Content-Type': 'application/json',
+				},
+data: {
+}
+};
+axios
+  .request(options)
+      .then(function (response) {
+      console.log(response.data);
+  })
+  .catch(function (error) {
+    console.error(error);
+  });
+
+
+})
+
 
 app.listen(process.env.PORT,()=>{
     console.log(`listening on port ${process.env.PORT}`);
