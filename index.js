a677238c909b43f4aafd8484e2f4bc380921ec46
shivani <2020ume0227@iitjammu.ac.in>
2024-04-23 03:22:10 +0530

    payload-added

--- index.js
+++ index.js
@@ -1,44 +1,54 @@
-import express from 'express';
-import 'dotenv/config';
+import express from "express";
+import "dotenv/config";
 import axios from "axios";
-
+import uniqid from "uniqid";
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
 
+app.get("/pay", (req, res) => {
+  const payEnd = "/pg/v1/pay";
+  const MERCHANT_TRANCTION_ID = uniqid();
+  const user_id = "123";
 
-app.get("/",(req,res)=>{
-    res.send("hello world");
-})
+  const payload = {
+    merchantId: process.env.Merchant_ID,
+    merchantTransactionId: MERCHANT_TRANCTION_ID,
+    merchantUserId: user_id,
+    amount: 100,
+    redirectUrl: `http://localhost:3000/redirect-url/${MERCHANT_TRANCTION_ID}`,
+    redirectMode: "REDIRECT",
+    mobileNumber: "9999999999",
+    paymentInstrument: {
+      type: "PAY_PAGE",
+    },
+  };
 
-app.get("/pay",(req,res)=>{
-    const payEnd = "/pg/v1/pay";
+//   "SHA256(base64 encoded payload + "/pg/v1/pay" + salt key) + ### + salt index"
 
+const x_verify = ;
 
-    
-
-const options = {
-  method: 'post',
-  url: `${process.env.PHONE_PE_HOST_URL}${payEnd}`,
-  headers: {
-        accept: 'text/plain',
-        'Content-Type': 'application/json',
-				},
-data: {
-}
-};
-axios
-  .request(options)
-      .then(function (response) {
+  const options = {
+    method: "post",
+    url: `${process.env.PHONE_PE_HOST_URL}${payEnd}`,
+    headers: {
+      accept: "text/plain",
+      "Content-Type": "application/json",
+    },
+    data: {},
+  };
+  axios
+    .request(options)
+    .then(function (response) {
       console.log(response.data);
-  })
-  .catch(function (error) {
-    console.error(error);
-  });
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+});
 
-
-})
-
-
-app.listen(process.env.PORT,()=>{
-    console.log(`listening on port ${process.env.PORT}`);
-})
+app.listen(process.env.PORT, () => {
+  console.log(`listening on port ${process.env.PORT}`);
+});
