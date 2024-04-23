df20f6fd3f28b081460cf0232e5a23280b3965e7
shivani <2020ume0227@iitjammu.ac.in>
2024-04-23 07:12:28 +0530

    traction-url-added-and-testing

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from "express";
 import "dotenv/config";
 import axios from "axios";
 import uniqid from "uniqid";
+import sha256 from "sha256";
+
 const app = express();
 
 app.get("/", (req, res) => {
@@ -26,29 +28,47 @@ app.get("/pay", (req, res) => {
     },
   };
 
-//   "SHA256(base64 encoded payload + "/pg/v1/pay" + salt key) + ### + salt index"
+    
+  //   "SHA256(base64 encoded payload + "/pg/v1/pay" + salt key) + ### + salt index"
+  const bufferObj = Buffer.from(JSON.stringify(payload), "utf-8");
+  const base64EncodedPayload = bufferObj.toString("base64");
 
-const x_verify = ;
+  const x_verify =
+    sha256(base64EncodedPayload + payEnd + process.env.SALT_KEY) +
+    "###" +
+    process.env.SALT_INDEX;
 
-  const options = {
-    method: "post",
-    url: `${process.env.PHONE_PE_HOST_URL}${payEnd}`,
-    headers: {
-      accept: "text/plain",
-      "Content-Type": "application/json",
-    },
-    data: {},
-  };
+    const options = {
+      method: "post",
+      url: `${process.env.PHONE_PE_HOST_URL}${payEnd}`,
+      headers: {
+        accept: "application/json", // Change to application/json
+        "Content-Type": "application/json",
+        "X-VERIFY": x_verify,
+      },
+      data: {
+        request: base64EncodedPayload,
+      },
+    };
   axios
     .request(options)
     .then(function (response) {
       console.log(response.data);
+      const url = response.data.data.instrumentResponse.redirectInfo.url;
+      res.send(url);
     })
     .catch(function (error) {
       console.error(error);
     });
 });
 
+app.get("/redirect-url/:merchantId",(req,res)=>{
+  const {merchantTransactionId} = req.params;  
+  res.send()
+})
+
+
 app.listen(process.env.PORT, () => {
   console.log(`listening on port ${process.env.PORT}`);
+  
 });
